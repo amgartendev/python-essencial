6dd25a4b1033a849d24787b2ade4835d703a36f6
João Amgarten <joaoamgarten0@gmail.com>
2022-07-01 02:23:39 +0100

    Eleventh Commit

---
+++ randomTests.py
@@ -0,0 +1,16 @@
+def seja_educado_mesmo(funcao):
+    def sendo_mesmo():
+        print('Foi um prazer conhecer você')
+        funcao()
+        print('Tenha um excelente dia!')
+    return sendo_mesmo
+
+
+@seja_educado_mesmo
+def apresentando():
+    print('Meu nome é Pedro')
+
+
+# Testando
+
+apresentando()
