77df809de1cf61e172c9d3020d5238c7de28ea2e
João Amgarten <joaoamgarten0@gmail.com>
2022-07-25 13:26:42 +0100

    Twenty-Sixth Commit

--- randomTests.py
+++ randomTests.py
@@ -1,71 +1,42 @@
-class Account:
-    counter = 0
-
-    def __init__(self, owner, balance, limit):
-        self.__counter = Account.counter
-        self.__owner = owner
-        self.__balance = balance
-        self.__limit = limit
-        Account.counter += 1
-
-    @property
-    def owner(self):
-        return self.__owner
-
-    @owner.setter
-    def owner(self, new_owner):
-        self.__owner = new_owner
-
-    @property
-    def balance(self):
-        return self.__balance
-
-    @balance.setter
-    def balance(self, new_balance):
-        self.__balance = new_balance
-
-    @property
-    def limit(self):
-        return self.__limit
-
-    @limit.setter
-    def limit(self, new_limit):
-        self.__limit = new_limit
-
-    def show_infos(self):
-        return f'{self.__owner} has the balance of {self.__balance} and a limit of {self.__limit}'
-
-    def transfer(self, amount, recipient):
-        self.__balance -= amount
-        recipient.__balance += amount
+# Herança e Polimorfismo
 
 
-acc1 = Account('João', 2000, 3000)
-acc2 = Account('Roberto', 5000, 7000)
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
 
-print(acc1.owner)
-print(acc1.balance)
-print(acc1.limit)
+    def nome_completo(self):
+        return f'{self.__nome} {self.__sobrenome}'
 
-print(acc1.show_infos())
+    def cumprimentar(self):
+        return f'{self.__nome} está te cumprimentando!'
 
-acc1.owner = 'Carlos'
-acc1.balance = 1000
-acc1.limit = 2000
 
-print(acc1.owner)
-print(acc1.balance)
-print(acc1.limit)
+class Cliente(Pessoa):
+    def __init__(self, nome, sobrenome, numero_cliente):
+        super().__init__(nome, sobrenome)
+        self.__numero_cliente = numero_cliente
 
-print(acc1.show_infos())
+    def cumprimentar(self):
+        return f'Olá, eu sou o cliente {self._Pessoa__nome}'
 
-print('=' * 15)
 
-print(acc1.__dict__)
-print(acc2.__dict__)
+class Funcionario(Pessoa):
+    def __init__(self, nome, sobrenome, licenca):
+        super().__init__(nome, sobrenome)
+        self.__licenca = licenca
 
-acc1.transfer(500, acc2)
-print('-' * 15)
+    def cumprimentar(self):
+        return f'Olá, eu sou o funcionário {self._Pessoa__nome}'
 
-print(acc1.__dict__)
-print(acc2.__dict__)
+
+cliente1 = Cliente('Fulano', 'Ciclano', 'BA321')
+print(cliente1.nome_completo())
+print(cliente1.cumprimentar())
+
+funcionario1 = Funcionario('Joao', 'Amgarten', 'AB123')
+print(funcionario1.nome_completo())
+print(funcionario1.cumprimentar())
+
+print(funcionario1)
