08440edd47617c5983872c0a058c7a40d6cc003a
João Amgarten <joaoamgarten2003@gmail.com>
2022-08-20 01:37:14 +0100

    Fifty-Third Commit

---
+++ 167-DebuggerComFStrings.py
@@ -0,0 +1,14 @@
+"""
+Debugger mais simples com f strings
+
+Com o novo recurso f string nós podemos facilmente debuggar valores ou
+expressões bastando adicionar o nome da variável juntamente com o sinal
+de igual
+
+Exemplo:
+geek: str = 'John Mayers'
+
+print(f'{geek=}')
+
+print(f'{geek.upper()[::-1]=}')
+"""
