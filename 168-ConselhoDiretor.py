cfc5b3b3e6285634262cd1e5d72e0cc27746c84d
João Amgarten <joaoamgarten2003@gmail.com>
2022-08-20 01:57:34 +0100

    Fifty-Fourth Commit

---
+++ 168-ConselhoDiretor.py
@@ -0,0 +1,12 @@
+"""
+Conselho Diretor
+
+Por trás do Python existe uma fundação chamada Python Software Foundation, essa fundação
+sempre foi gerenciada pelo criado da linguagem, o Guido van Rossum.
+
+A versão 3.8 do Python é a primeira versão da linguagem que não foi desenvolvida sob os
+'cuidados' do criador. Todas as outras versões eram guiadas e definidas por ele.
+
+A partir dessa versão, os rumos da linguagem Python são definidos por um grupo conselheiro
+composto por 5 pessoas, incluindo o próprio Guido van Rossum.
+"""
